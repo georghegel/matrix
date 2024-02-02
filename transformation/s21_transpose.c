5bf883f029882e451dac24f633ec9e9e218f403a
Rachel Sanford <rachelsa@va-g5.kzn.21-school.ru>
2024-02-02 14:37:51 +0300

    ADDED: memory allocation to the resulting variable

--- transformation/s21_transpose.c
+++ transformation/s21_transpose.c
@@ -1,14 +1,15 @@
 #include "../s21_matrix.h"
 
 int s21_transpose(matrix_t *A, matrix_t *result) {
-
+    int answer = OK;
     if (!does_matrix_exist(result) || !does_matrix_exist(A))
-        return FAILURE;
+        answer = INCORRECT_MATRIX;
 
-    for (int i = 0; i < A->rows; ++i) {
-        for (int j = 0; j < A->columns; ++j) {
-            result->matrix[j][i] = A->matrix[i][j];
+    s21_create_matrix(A->columns, A->rows, result);
+    for (int i = 0; i < result->rows; ++i) {
+        for (int j = 0; j < result->columns; ++j) {
+            result->matrix[i][j] = A->matrix[j][i];
         }
     }
-    return SUCCESS;
+    return answer;
 }
