a10362bc7479586625ef6dd46e6603a4f329e721
Rachel Sanford <rachelsa@va-g5.kzn.21-school.ru>
2024-02-02 14:37:24 +0300

    Minor fixes

--- s21_matrix.h
+++ s21_matrix.h
@@ -23,22 +23,22 @@ int s21_create_matrix(int rows, int columns, matrix_t *result);     // DONE
 void s21_remove_matrix(matrix_t *A);                                // DONE
 int s21_eq_matrix(matrix_t *A, matrix_t *B);                        // DONE
 
-int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
-int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
-int s21_mult_number(matrix_t *A, double number, matrix_t *result);
-int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
+int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result);     // DONE // CREATE RESULT MATRIX EVERYWHERE BELOW //
+int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result);     // DONE
+int s21_mult_number(matrix_t *A, double number, matrix_t *result);  // DONE
+int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result);    // DONE
 
-int s21_transpose(matrix_t *A, matrix_t *result);
+int s21_transpose(matrix_t *A, matrix_t *result);                   // DONE
 
-int s21_calc_complements(matrix_t *A, matrix_t *result);
+int s21_calc_complements(matrix_t *A, matrix_t *result);            //NOT DONE                  HERE
 int s21_determinant(matrix_t *A, double *result);
 int s21_inverse_matrix(matrix_t *A, matrix_t *result);
 
 // TOOLS
-int is_equal_size(matrix_t *m1, matrix_t *m2);
-int does_matrix_exist(matrix_t *matrix);
-int is_square_matrix(matrix_t *matrix);
-int check_rows_columns(int rows, int columns);
+int is_equal_size(matrix_t *m1, matrix_t *m2);                      // DONE
+int does_matrix_exist(matrix_t *matrix);                            // DONE
+int is_square_matrix(matrix_t *matrix);                             // DONE
+int check_rows_columns(int rows, int columns);                      // DONE
 
 double argmax(int begin, int end, matrix_t *matrix);
 void row_copy(double *term1, double *term2, int columns);
--- tools/is_square_matrix.c
+++ tools/is_square_matrix.c
@@ -1,5 +1,5 @@
 #include "../s21_matrix.h"
 
 int is_square_matrix(matrix_t *matrix){
-    return (matrix->columns != matrix->rows) ? FAILURE : SUCCESS;
+    return (matrix->columns == matrix->rows) ? SUCCESS : FAILURE;
 }
