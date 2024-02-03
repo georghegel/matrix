1fb4e4e3fe78488d8121e7f266ccad0088f226b2
Rachel Sanford <rachelsa@va-g5.kzn.21-school.ru>
2024-02-03 16:32:36 +0300

    Added function for determinant

---
+++ tools/find_non_zero_diagonal_element.c
@@ -0,0 +1,10 @@
+#include "../s21_matrix.h"
+
+int find_non_zero_diagonal_element(int start, matrix_t *matrix){
+    int answer = start - 1;
+    for (int i = start; i < matrix->rows && answer == start - 1; ++i) {
+        if (matrix->matrix[i][i] != 0.0)
+            answer = i;
+    }
+    return answer;
+}
